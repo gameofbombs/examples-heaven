706534e9cec34e41b0423ce7bbb841ed0dccc418
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2018-02-15 12:47:04 +0300

    invert the colors

--- required/examples/basics/basic.js
+++ required/examples/basics/basic.js
@@ -1,11 +1,16 @@
-var app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
+let app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
 document.body.appendChild(app.view);
 
 // create a new Sprite from an image path
-var bunny = PIXI.Sprite.fromImage('required/assets/basics/bunny.png')
+let bunny = new PIXI.heaven.Sprite(PIXI.Texture.fromImage('required/assets/basics/bunny.png'));
+
+// Let us invert the colors!
+bunny.color.setLight(0.0, 0.0, 0.0);
+bunny.color.setDark(1.0, 1.0, 1.0);
 
 // center the sprite's anchor point
 bunny.anchor.set(0.5);
+bunny.scale.set(3.0);
 
 // move the sprite to the center of the screen
 bunny.x = app.screen.width / 2;
